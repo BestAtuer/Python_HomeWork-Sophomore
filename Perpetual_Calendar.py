2b206c8bbfe893212338563dcbc10df015092780
Atuer <49964852+BestAtuer@users.noreply.github.com>
2022-10-12 09:27:14 +0800

    Update Perpetual_Calendar.py

--- Perpetual_Calendar.py
+++ Perpetual_Calendar.py
@@ -8,14 +8,10 @@ def is_leap_year(y):
     return False
 # 今年此月前日期数
 def days_before(y,m):
-    if is_leap_year(y):
-        return (y-1)*365+M.floor((y-1)/4)-M.floor((y-1)/100)+M.floor((y-1)/400)+days_qzh[m-1]+1
-    return (y-1)*365+M.floor((y-1)/4)-M.floor((y-1)/100)+M.floor((y-1)/400)+days_qzh[m-1]
+    return (y-1)*365+M.floor((y-1)/4)-M.floor((y-1)/100)+M.floor((y-1)/400)+days_qzh[m-1]+(is_leap_year(y) and m>2)
 # 本月日期数
 def days_in_month(y,m):
-    if is_leap_year(y) and m==2:
-        return 29
-    return days_qzh[m]-days_qzh[m-1]
+    return days_qzh[m]-days_qzh[m-1]+(is_leap_year(y) and m==2)
 #主程序
 y = int(input("请输入年份>>"))
 m = int(input("请输入月份>>"))
